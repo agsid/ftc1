15449a2a0da6f7f33f1822f7534f67ca49dc5ce6
SID AG <121702864+agsid@users.noreply.github.com>
2025-06-24 23:00:45 -0400

    Update Main.java

--- learn/Car/Main.java
+++ learn/Car/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         car myCar = new car("Aston Martin", 900000);
 
-        System.out.println(myCar.oldOrNew());
+        System.out.println("This car is a " + myCar.oldOrNew()+ " car.");
 
         base myPorsche = new porsche();
         myPorsche.type();
