85cd2f70eba53af7c15192264933d7f248ff260d
SID AG <121702864+agsid@users.noreply.github.com>
2025-06-24 20:05:23 -0400

    Create audi.java

---
+++ audi.java
@@ -0,0 +1,13 @@
+public class porsche implements base {
+
+    @Override
+    public void company() {
+       System.out.println("Audi");
+    }
+
+    @Override
+    public void model() {
+       System.out.println("A6");
+    }
+
+}
